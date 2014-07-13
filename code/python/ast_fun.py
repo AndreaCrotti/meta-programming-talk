c72b7a69854b5937e17ea01ff26f5936a9f46bbc
Andrea Crotti <andrea.crotti.0@gmail.com>
2014-07-13 16:45:24 +0100

    finish ast example

--- code/python/ast_fun.py
+++ code/python/ast_fun.py
@@ -9,11 +9,12 @@ def remove_hello():
     import inspect
     source = inspect.getsource(function_hello)
     ast_tree = ast.parse(source)
-    st = ast_tree.body[0].body[0].value.Argos[0]
+    st = ast_tree.body[0].body[0].value.args[0]
     st.s = st.s.replace('hello ', '')
     return compile(ast_tree, __file__, mode='exec')
 
 
 if __name__ == '__main__':
     function_hello()
-    remove_hello()
+    exec(remove_hello())
+    function_hello()
